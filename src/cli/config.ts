6438a640ef340b95773991114a3bcef33ee967fc
dkoujelev <dkoujelev97@gmail.com>
2021-04-11 16:34:30 +0200

    Create cli and add new config options

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,25 +1,21 @@
-import { Config } from './../types';
+import { Config } from '../types';
 import path from 'path';
 import fs from 'fs';
 import configTemplate from './changelogConfigTemplate.json';
 import chalk from 'chalk';
 
-export const getOrCreateConfig = (): Config => {
+export const getConfig = (): Config | null => {
   const configPath = path.join(process.cwd(), './changelogConfig.json');
   const configFound = fs.existsSync(configPath);
 
-  let config: Config;
-
-  if (configFound) {
-    config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
-  } else {
-    config = createConfig();
+  if (!configFound) {
+    return null;
   }
 
-  return config;
+  return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
 };
 
-const createConfig = () => {
+export const createConfigFile = (): void => {
   const config: Config = configTemplate;
 
   fs.appendFile('changelogConfig.json', JSON.stringify(config, null, 2), (err) => {
@@ -28,11 +24,8 @@ const createConfig = () => {
       console.error(err);
       process.exit(1);
     } else {
-      console.log(chalk.blueBright('Created config file'));
-      console.log(chalk.blue('Please update the config file and try again.'));
-      process.exit(0);
+      console.log(chalk.blueBright('Created config file.'));
+      console.log(chalk.blue('Please update the config file and run the command again.'));
     }
   });
-
-  return config;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-import { getOrCreateConfig } from './cli/getConfig';
-import { generateChangelog } from './generateChangelog';
-import { getMergedPullRequests } from './getMergedPullRequests';
-import chalk from 'chalk';
-import clear from 'clear';
-import figlet from 'figlet';
-import { Spinner } from 'clui';
+import { runCli } from './cli';
 
-const run = async () => {
-  clear();
-
-  const config = getOrCreateConfig();
-
-  console.log(chalk.green(figlet.textSync('Changelog generator', { horizontalLayout: 'full' })));
-
-  const spinner = new Spinner('Fetching pull requests');
-  spinner.start();
-
-  const pullRequests = await getMergedPullRequests(config);
-
-  spinner.stop();
-
-  console.log(chalk.greenBright('Pull requests fetched \n'));
-
-  console.log(generateChangelog(config, pullRequests));
-};
-
-run();
+runCli();
